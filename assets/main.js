71bfa0f8dfde909b2218e22227480d3aa824d088
Rob Dominguez <rtjdominguez@gmail.com>
2022-03-14 12:37:15 -0500

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,36 @@
+// Sample info
+const sampleInfo = [
+  // group one's stuff
+  {
+    header: `We're going to manipulate this page via JavaScript`,
+    body: `
+    <p>We can even write HTML and use it dynamically. Don't believe me? <a href="https://www.google.com" target="_blank">Check out this link.</a>
+  `,
+    groupOneImagePath: "./assets/pretty-lake.jpg"
+  },
+  // group two's stuff
+  {
+    meals: [{
+        name: `Chicken Fingers`,
+        desc: `Tasty, fried, yellow meal.`,
+        dayOffered: `Monday`,
+        img: `./assets/chicken.jpg`
+      },
+      {
+        name: `Pizza`,
+        desc: `Round. Delicious. Italian?`,
+        dayOffered: `Tuesday`,
+        img: `./assets/pizza.jpg`
+      },
+      {
+        name: `Super Burger Bar`,
+        desc: `Make it yourself! It's your fault if you don't like it.`,
+        dayOffered: `Wednesday`,
+        img: `./assets/burger.jpg`
+      }
+    ]
+  },
+]
+
+// Do your work below! Remember, you only need to write the snippets for your group's work.
+// However, you should take notes on all groups' assignments.
